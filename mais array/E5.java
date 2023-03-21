0980831cc898a1a3fda67e96d7ad02a3db6a7245
Bernardo P Zapelini <94933673+bino1432@users.noreply.github.com>
2023-03-20 21:31:24 -0300

    Add files via upload

---
+++ mais array/E5.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class E5 {
+    public static void main (String []args){
+
+        Random rand = new Random();
+        int [] valor = new int [10];
+        int digito;
+
+        Scanner leia = new Scanner(System.in);
+        for(int cont = 0; cont < valor.length;cont++){
+            valor[cont] = rand.nextInt(100);
+        }
+        System.out.print("insira um valor: ");
+        digito = leia.nextInt();
+        for (int cont = 1; cont < valor.length;cont++){
+            if (digito == valor[cont]){
+                System.out.print("o numero está na casa: "+cont);
+                break;
+            }
+        }
+        System.out.print("você não acertou");
+        leia.close();
+    }
+}
